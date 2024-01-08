60d9f83226db821d6e8b58db3608c57cf15274eb
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-01-08 00:51:17 -0800

    Update render

--- 03_render_chapters.R
+++ 03_render_chapters.R
@@ -13,3 +13,4 @@ make_esr_chapter(xml_path = "./chapters/GOA_forage_fish.xml")
 make_esr_chapter(xml_path = "./chapters/EBS_jellyfish.xml")
 make_esr_chapter(xml_path = "./chapters/EBS_misc_species.xml")
 make_esr_chapter(xml_path = "./chapters/EBS_structural_epifauna.xml")
+
