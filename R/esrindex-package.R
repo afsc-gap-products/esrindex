5ebc4678aac8efca958cc183d43467f8cc02cb8d
sean-rohan-NOAA <sean.rohan@noaa.gov>
2024-10-01 13:40:32 -0700

    Fix build error

--- R/esrindex-package.R
+++ R/esrindex-package.R
@@ -1,7 +1,7 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' @import dplyr ggplot2 RODBC getPass knitr rmarkdown rema scales here akgfmaps sf ragg viridis
+#' @import dplyr ggplot2 RODBC getPass knitr rmarkdown rema scales here akgfmaps sf ragg viridis classInt cowplot
 #' @importFrom grDevices dev.off
 #' @importFrom methods is
 #' @importFrom stats sd quantile
